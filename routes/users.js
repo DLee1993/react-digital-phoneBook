5aaf69c5b5dae0a100fdc2385051c108f44e2dd9
DLee1993 <Dai180293@gmail.com>
2022-11-03 11:57:26 +0000

    added user to user collection

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 //! THIS ROUTE IS TO INITALLY SIGN UP / ADD A NEW USER
 const express = require("express");
-const user = require("../models/User");
+const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
+const { genSalt, hash } = require("bcryptjs");
 
 const router = express.Router();
 
@@ -16,12 +17,26 @@ router.post(
         check("email", "Please enter a valid email").isEmail(),
         check("password", "Please enter a password with 6 or more characters").isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()})
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-        res.send("passed");
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            user ? res.status(400).json({ error: "User already exists" }) : null;
+            user = new User({ name, email, password });
+            const salt = await genSalt(10)
+            user.password = await hash(password, salt); 
+            await user.save()
+            res.send('user saved')
+        } catch (error) {
+            console.error(error.msg)
+            res.status(500).send('server error')
+        }
     }
 );
 
