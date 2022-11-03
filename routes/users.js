f1b38e279fb15c9ce00e6e5fdfdf681bd3436e0b
DLee1993 <Dai180293@gmail.com>
2022-11-03 16:50:20 +0000

    added jwt to generate a token for each userr

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const express = require("express");
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 const { genSalt, hash } = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const router = express.Router();
 
@@ -29,13 +31,28 @@ router.post(
             let user = await User.findOne({ email });
             user ? res.status(400).json({ error: "User already exists" }) : null;
             user = new User({ name, email, password });
-            const salt = await genSalt(10)
-            user.password = await hash(password, salt); 
-            await user.save()
-            res.send('user saved')
+            const salt = await genSalt(10);
+            user.password = await hash(password, salt);
+            await user.save();
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                {
+                    expiresIn: 3600000,
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (error) {
-            console.error(error.msg)
-            res.status(500).send('server error')
+            console.error(error.msg);
+            res.status(500).send("server error");
         }
     }
 );
