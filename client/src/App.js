1ca384db5f33862d73472c301030ce96b42e5847
DLee1993 <Dai180293@gmail.com>
2022-11-04 19:28:41 +0000

    added in contact initial state and contact component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar";
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
 import "./App.css";
 
 const App = () => {
     return (
-        <Router>
-          <Fragment className='App'>
-            <Navbar />
-            <div className="container">
-              <Routes>
-                <Route exact path='/' element={<Home/>}></Route>
-                <Route exact path='/about' element={<About/>}></Route>
-              </Routes>
-            </div>
-        </Fragment>
-        </Router>
+        <ContactState>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <div className='container'>
+                        <Routes>
+                            <Route exact path='/' element={<Home />}></Route>
+                            <Route exact path='/about' element={<About />}></Route>
+                        </Routes>
+                    </div>
+                </Fragment>
+            </Router>
+        </ContactState>
     );
 };
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Contacts from "../contacts/Contacts";
 
 const Home = () => {
-  return (
-    <div><h1>Home</h1></div>
-  )
-}
+    return (
+        <div className='grid-2'>
+            {/* Contact form */}
+            <div></div>
+            {/* Contacts list */}
+            <div><Contacts/></div>
+        </div>
+    );
+};
 
 export default Home;
