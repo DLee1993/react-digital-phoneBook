ea5ec781430e37507107c399de792252fb2254c8
DLee1993 <Dai180293@gmail.com>
2022-11-11 16:06:27 +0000

    added the login and register components, also added the routes/links for both

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
     return (
@@ -20,6 +22,8 @@ const App = () => {
                                 {/* The below routes lead to a page */}
                                 <Route exact path='/' element={<Home />}></Route>
                                 <Route exact path='/about' element={<About />}></Route>
+                                <Route exact path='/register' element={<Register />}></Route>
+                                <Route exact path='/login' element={<Login />}></Route>
                             </Routes>
                         </div>
                     </Fragment>
